f9fe9609494a8a2a40179f3c2a7a522a4774db30
Jose Wills <jose.wills3@gmail.com>
2023-06-30 21:16:55 +0100

    added properties and addProperty components, added router and routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "../styles/app.css";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
-const App = () => (
-  <div className="App">
-    <h1>Surreal Estate</h1>
-    <NavBar />
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Surreal Estate</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Properties />} />
+        <Route path="/add-property" element={<AddProperty />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "../styles/navbar.css";
+import { Link } from "react-router-dom";
 import logo from "../images/logo.jpg";
 
 const NavBar = () => {
   return (
     <div className="navbar">
-      <img src={logo} alt="Logo" className="navbar-logo" />
-      <ul className="navbar-links">
-        <li className="navbar-links__item">View Properties</li>
-        <li className="navbar-links__item">Add a Property</li>
+      <img src={logo} alt="Logo" className="navbar__logo" />
+      <ul className="navbar__links">
+        <li className="navbar__links__item">
+          <Link to="/">View Properties</Link>
+        </li>
+        <li className="navbar__links__item">
+          <Link to="/add-property">Add a Property</Link>
+        </li>
       </ul>
     </div>
   );
