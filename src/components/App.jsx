c7af68836ab6c6302f81b51c5fac1f09655bfab8
Jose Wills <jose.wills3@gmail.com>
2023-07-01 11:25:56 +0100

    added furtger styling and updated layout slightly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,15 @@ import { Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
+import logo from "../images/logo.jpg";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Surreal Estate</h1>
+      <div className="App__container">
+        <img src={logo} alt="Logo" className="App__logo" />
+        <h1>Surreal Estate</h1>
+      </div>
       <NavBar />
       <Routes>
         <Route path="/" element={<Properties />} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
-import logo from "../images/logo.jpg";
 
 const NavBar = () => {
   return (
     <div className="navbar">
-      <img src={logo} alt="Logo" className="navbar__logo" />
       <ul className="navbar__links">
         <li className="navbar__links__item">
           <Link to="/">View Properties</Link>
