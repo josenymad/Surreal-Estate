463b924a884209927c4b309805412dcee3d720b9
Jose Wills <jose.wills3@gmail.com>
2023-07-02 11:14:54 +0100

    more form styling, added tests for app.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,40 @@
-import { render, screen } from "@testing-library/react";
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "../components/App";
 
-test('renders "Surreal Estate"', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/surreal estate/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders 'Surreal Estate'", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const appHeader = screen.getByText("Surreal Estate");
+
+    expect(appHeader).toBeInTheDocument();
+  });
+
+  it("renders logo", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const logo = screen.getByAltText("Logo");
+
+    expect(logo).toBeInTheDocument();
+  });
 });
